9418aee9ad7f2c4ec514f4ffd2513ab426e9bf92
dileep83 <dileep@Administrators-MacBook-Pro-5.local>
2023-03-07 13:34:46 +0530

    df

--- list/binary_search.py
+++ list/binary_search.py
@@ -13,5 +13,5 @@ def binary_search(list, item):
 
 
 if __name__ == '__main__':
-    list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    print(binary_search(list, 10))
+    list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,543,64568,7859]
+    print(binary_search(list, 543))
