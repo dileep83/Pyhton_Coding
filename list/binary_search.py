4fe8e171ec8c7d3527e7c8b6592e48a36c598351
dileep83 <dileep@Administrators-MacBook-Pro-5.local>
2023-03-07 13:09:34 +0530

    binary search function is added

---
+++ list/binary_search.py
@@ -0,0 +1,17 @@
+def binary_search(list, item):
+    low = 0
+    high = len(list) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if list[mid] == item:
+            return mid
+        elif list[mid] < item:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1
+
+
+if __name__ == '__main__':
+    list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    print(binary_search(list, 10))
