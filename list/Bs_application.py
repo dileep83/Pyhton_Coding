8ec40e20d2d3b7447a2f66159e5034a5d6cd9498
dileep83 <dileep@Administrators-MacBook-Pro-5.local>
2023-03-14 13:28:17 +0530

    solved 2 question

---
+++ list/Bs_application.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left = 0
+        right = len(nums)-1
+        if len(nums) == 1 and target == nums[0]:
+            return 0
+        if target == nums[-1] :
+            return len(nums)-1
+        if target > nums[-1] :
+            return len(nums)
+        if target <= nums[0]:
+            return 0
+        while(left <= right):
+            mid = (left +right)// 2
+            if nums[mid] == target and nums[mid+1] > target :
+                return mid
+            elif  target > nums[mid] :
+                left = mid+1
+            else:right = mid-1
+        return right+1
--- list/binary_search.py
+++ list/binary_search.py
@@ -1,3 +1,4 @@
+from Bs_application import Solution
 def binary_search(list, item):
     low = 0
     high = len(list) - 1
@@ -15,3 +16,6 @@ def binary_search(list, item):
 if __name__ == '__main__':
     list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,543,64568,7859]
     print(binary_search(list, 543))
+    s = Solution()
+    print(s.searchInsert([1,3],3)) 
+    
